59c42976d63885708e884604595ac1f0c024278f
KUMARI ANANYA <75521183+104ananya@users.noreply.github.com>
2023-02-03 21:10:08 +0530

    name change

--- stromwise/src/App.js
+++ stromwise/src/App.js
@@ -4,11 +4,23 @@ import Forecast from './components/Forecast';
 import Inputs from './components/Inputs';
 import TimeLoc from './components/TimeLoc';
 import TopButtons from './components/TopButtons';
+import getFormattedWeatherData from './weather/weather';
 
 
+// import getWeather from './weather/weather';
 // import UilReact from '@iconscout/react-unicons/icons/uil-react'
 
 function App() {
+
+  const fetchWeather = async () => {
+    const data = await getFormattedWeatherData({q: "london"});
+    console.log(data);
+  }
+
+  fetchWeather();
+
+
+
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 h-fit shadow-xl bg-gradient-to-br from-cyan-700 to-blue-700 ">
       <TopButtons />
--- stromwise/src/weather/weather.js
+++ stromwise/src/weather/weather.js
@@ -1,2 +1,61 @@
 const API_KEY = "723d7812c22a56e460d2d4bc9caf3b3a";
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
+
+const getWeather = async (infoType, searchParams) => {
+  const url = new URL(BASE_URL + "/" + infoType);
+  url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
+
+  //   console.log(url);
+
+  const res = await fetch(url);
+  return await res.json();
+  // .then((data) => data);
+};
+
+// export default getWeather;
+
+const formatCurrentWeather = (data) => {
+  const {
+    coord: { lat, lon },
+    main: { temp, feels_like, temp_min, temp_max, pressure, humidity },
+    visibility,
+    name,
+    dt,
+    sys: { country, sunrise, sunset },
+    weather,
+    wind: { speed },
+  } = data;
+
+  const { main: details, icon } = weather[0];
+
+  return {
+    lat,
+    lon,
+    temp,
+    feels_like,
+    temp_min,
+    temp_max,
+    pressure,
+    humidity,
+    visibility,
+    name,
+    dt,
+    country,
+    sunrise,
+    sunset,
+    details,
+    icon,
+    speed,
+  };
+};
+
+const getFormattedWeatherData = async (searchParams) => {
+  const formattedCurrentWeather = await getWeather(
+    "weather",
+    searchParams
+  ).then(formatCurrentWeather);
+
+  return formattedCurrentWeather;
+};
+
+export default getFormattedWeatherData;
