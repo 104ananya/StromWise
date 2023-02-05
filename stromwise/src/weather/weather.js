551b0c17d90f5b6f87747ddbb5bea67c6bc16cc8
KUMARI ANANYA <75521183+104ananya@users.noreply.github.com>
2023-02-05 08:36:53 +0530

    Update weather.js

--- stromwise/src/weather/weather.js
+++ stromwise/src/weather/weather.js
@@ -1,6 +1,6 @@
 import { DateTime } from "luxon";
 
-const API_KEY = "723d7812c22a56e460d2d4bc9caf3b3a";
+const API_KEY = "";
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
 const getWeather = async (infoType, searchParams) => {
